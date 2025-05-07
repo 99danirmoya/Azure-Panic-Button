3f488700c5d1d71815c20e43847952482f4dec4f
Dani Rguez Moya <119592616+99danirmoya@users.noreply.github.com>
2025-05-07 21:49:08 +0200

    Add files via upload

---
+++ CCFADevice/iot_configs.h
@@ -0,0 +1,24 @@
+/*****************************************************************************************************************************
+**********************************    Author  : Ehab Magdy Abdullah                      *************************************
+**********************************    Linkedin: https://www.linkedin.com/in/ehabmagdyy/  *************************************
+**********************************    Youtube : https://www.youtube.com/@EhabMagdyy      *************************************
+******************************************************************************************************************************/
+
+// Enter Your Wifi and Password
+// #define IOT_CONFIG_WIFI_SSID "WiFi-Rguez-Moya"
+// #define IOT_CONFIG_WIFI_PASSWORD "Trece131313!"
+#define IOT_CONFIG_WIFI_SSID "Pixel_OF13"
+#define IOT_CONFIG_WIFI_PASSWORD "mynameisjeff"
+// #define IOT_CONFIG_WIFI_SSID "DIGIFIBRA-24-h3JA"
+// #define IOT_CONFIG_WIFI_PASSWORD "edaXG9qJA6"
+
+
+// Azure IoT Hub
+#define IOT_CONFIG_IOTHUB_FQDN "CCIoTHubMoya.azure-devices.net"
+// Azure IoT Gub Device ID
+#define IOT_CONFIG_DEVICE_ID "CCIoTFADevice"
+// Azure IoT Hub Device Key (primary key)
+#define IOT_CONFIG_DEVICE_KEY "x0dXQ3yrtzIulqrSdkoEMXABsGgaa/rM1GRjXP+Kt3I="
+
+// Publish 1 message every 5 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 5000
