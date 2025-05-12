e02a35b500256eef33fa48e078898b0a1512d1e8
Dani Rguez Moya <119592616+99danirmoya@users.noreply.github.com>
2025-05-12 12:13:56 +0200

    Update iot_configs.h

--- CCFADevice/iot_configs.h
+++ CCFADevice/iot_configs.h
@@ -5,20 +5,16 @@
 ******************************************************************************************************************************/
 
 // Enter Your Wifi and Password
-// #define IOT_CONFIG_WIFI_SSID "WiFi-Rguez-Moya"
-// #define IOT_CONFIG_WIFI_PASSWORD "Trece131313!"
-#define IOT_CONFIG_WIFI_SSID "Pixel_OF13"
-#define IOT_CONFIG_WIFI_PASSWORD "mynameisjeff"
-// #define IOT_CONFIG_WIFI_SSID "DIGIFIBRA-24-h3JA"
-// #define IOT_CONFIG_WIFI_PASSWORD "edaXG9qJA6"
+#define IOT_CONFIG_WIFI_SSID "SSID"
+#define IOT_CONFIG_WIFI_PASSWORD "PASSWORD"
 
 
 // Azure IoT Hub
-#define IOT_CONFIG_IOTHUB_FQDN "CCIoTHubMoya.azure-devices.net"
+#define IOT_CONFIG_IOTHUB_FQDN "<IoT_Hub_Name>.azure-devices.net"
 // Azure IoT Gub Device ID
-#define IOT_CONFIG_DEVICE_ID "CCIoTFADevice"
+#define IOT_CONFIG_DEVICE_ID "DEVICE_ID"
 // Azure IoT Hub Device Key (primary key)
-#define IOT_CONFIG_DEVICE_KEY "x0dXQ3yrtzIulqrSdkoEMXABsGgaa/rM1GRjXP+Kt3I="
+#define IOT_CONFIG_DEVICE_KEY "DEVICE_KEY"
 
 // Publish 1 message every 5 seconds
 #define TELEMETRY_FREQUENCY_MILLISECS 5000
